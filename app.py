3bc2481eda9f8d2d567e5efda01c0f6500358c1b
dmmanchon <david.martin.manchon@gmail.com>
2025-06-24 19:31:52 +0200

    Funcionalidad eliminar enlaces compartidos (9)

--- app.py
+++ app.py
@@ -556,11 +556,12 @@ except:
 
 # Permitir eliminar los enlaces compartidos
 if enlaces_lista:
+    st.markdown("---")
     for i, (nombre, enlace) in enumerate(enlaces_lista):
         col1, col2 = st.columns([0.6, 0.4])  # 60% enlace - 40% espacio para botón
         with col1:
             st.markdown(f"""
-                <p style='font-size: 1.1rem; font-weight: bold; margin: 0;'>
+                <p style='font-size: 1.25rem; font-weight: 600; margin: 0 0 0.5rem 0;'>
                     🔗 <a href="{enlace}" target="_blank" style="text-decoration: none; color: #0066cc;">
                         {nombre}
                     </a>
